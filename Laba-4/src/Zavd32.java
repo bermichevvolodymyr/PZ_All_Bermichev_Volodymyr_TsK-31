0aae9ddf57e758df8f1773772ad6a9a83383e3f1
Бермічев Володимир ЦК-31 <bermichev.volodymyr@gmail.com>
2025-11-11 14:06:07 +0200

    Refactor sumEvenOnOddPositions to instance method

--- Laba-4/src/Zavd32.java
+++ Laba-4/src/Zavd32.java
@@ -1,5 +1,5 @@
 public class Zavd32 {
-    public static int sumEvenOnOddPositions(int[] array) {
+    public int sumEvenOnOddPositions(int[] array) {
         if (array == null) {
             throw new NullPointerException("Масив не може бути null");
         }
@@ -22,51 +22,18 @@ public class Zavd32 {
     }
 
     public static void main(String[] args) {
-
+        Zavd32 obj = new Zavd32();
         int[] test1 = {1, 4, 3, 6, 5, 8, 7};
         System.out.println("Тест 1: " + java.util.Arrays.toString(test1));
         System.out.println("Індекси непарні (1,3,5): елементи [4, 6, 8]");
-        System.out.println("Сума парних на непарних позиціях: " + sumEvenOnOddPositions(test1));
-        System.out.println();
-
-        // Нема парних на непарних
-        int[] test2 = {2, 1, 4, 3, 6, 5};
-        System.out.println("Тест 2: " + java.util.Arrays.toString(test2));
-        System.out.println("Індекси непарні (1,3,5): елементи [1, 3, 5] - всі непарні");
-        System.out.println("Сума парних на непарних позиціях: " + sumEvenOnOddPositions(test2));
-        System.out.println();
-
-        int[] test3 = {2, 4, 6, 8, 10};
-        System.out.println("Тест 3: " + java.util.Arrays.toString(test3));
-        System.out.println("Індекси непарні (1,3): елементи [4, 8]");
-        System.out.println("Сума парних на непарних позиціях: " + sumEvenOnOddPositions(test3));
-        System.out.println();
-
-        int[] test4 = {-1, -4, 3, -6, 5, 10};
-        System.out.println("Тест 4: " + java.util.Arrays.toString(test4));
-        System.out.println("Індекси непарні (1,3,5): елементи [-4, -6, 10]");
-        System.out.println("Сума парних на непарних позиціях: " + sumEvenOnOddPositions(test4));
-        System.out.println();
-
-        // 1 елемент
-        int[] test5 = {5};
-        System.out.println("Тест 5: " + java.util.Arrays.toString(test5));
-        System.out.println("Немає елементів на непарних індексах");
-        System.out.println("Сума парних на непарних позиціях: " + sumEvenOnOddPositions(test5));
-        System.out.println();
-
-        // Два елемента
-        int[] test6 = {1, 8};
-        System.out.println("Тест 6: " + java.util.Arrays.toString(test6));
-        System.out.println("Індекс непарний (1): елемент [8]");
-        System.out.println("Сума парних на непарних позиціях: " + sumEvenOnOddPositions(test6));
+        System.out.println("Сума парних на непарних позиціях: " + obj.sumEvenOnOddPositions(test1));
         System.out.println();
 
         // Пустий
         try {
             int[] test7 = {};
-            System.out.println("Тест 7: порожній масив");
-            sumEvenOnOddPositions(test7);
+            System.out.println("Порожній масив");
+            obj.sumEvenOnOddPositions(test7);
         } catch (IllegalArgumentException e) {
             System.out.println("Виняток: " + e.getMessage());
             System.out.println();
@@ -74,8 +41,8 @@ public class Zavd32 {
 
         // Null масив
         try {
-            System.out.println("Тест 8: null масив");
-            sumEvenOnOddPositions(null);
+            System.out.println("Null масив");
+            obj.sumEvenOnOddPositions(null);
         } catch (NullPointerException e) {
             System.out.println("Виняток: " + e.getMessage());
         }
